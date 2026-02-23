1d0244d5eef5a9d930f47fec6b0ddf79e9524863
Kevin Porras <kevin.porras@pantheon.io>
2026-02-23 16:27:33 -0600

    Use revalidatePath.

--- pages/api/revalidate-path.ts
+++ pages/api/revalidate-path.ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { revalidatePath } from 'next/cache';
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const path = req.query.path as string;
@@ -9,7 +11,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     console.log(`[RevalidatePath] Revalidating: ${path}`);
-    await res.revalidate(path);
+    revalidatePath(path);
 
     return res.json({
       message: `Path '${path}' revalidated`,
